363b2299e04522f35e73c079780d40275805807d
osemenyuk-114 <oleksandr.semenyuk@hotmail.com>
2024-11-10 10:53:50 +0100

    feat: build tools updated

--- ZX_RGBI_TO_VGA_HDMI/scripts/make.ps1
+++ ZX_RGBI_TO_VGA_HDMI/scripts/make.ps1
@@ -10,15 +10,17 @@ Param(
 # Arduino CLI executable name and config
 $ARDUINO_CLI = "arduino-cli.exe"
 $CONFIG_DIR = "$($env:USERPROFILE)\.arduinoIDE"
-# Arduino CLI Board type
-$FQBN = "rp2040:rp2040:rpipico"
 # Default port to upload to
 $PORT = "COM6"
+$BAUDRATE = "9600"
+$PORT_CONFIG = "--config baudrate=$BAUDRATE" -split "\s+"
 # Optional verbose compile/upload trigger
 $VERBOSE = "--verbose"
 # Exter build flags
 $ExtraBuildFlags = "build.flags.optimize=-O3" -split "\s+"
 # $ExtraBuildFlags += "compiler.c.extra_flags=-save-temps compiler.cpp.extra_flags=-save-temps" -split "\s+"
+# Common parameters
+$BaseParams = "--config-dir $CONFIG_DIR --port $PORT" -split "\s+"
 
 $BuildFlags = @()
 
@@ -30,17 +32,17 @@ foreach ($buildFlag in $ExtraBuildFlags) {
 $Params = @()
 
 if ($Action -eq "build") {
-    $Params = ("compile --config-dir $CONFIG_DIR $VERBOSE --port $PORT --fqbn $FQBN" -split "\s+") + $BuildFlags
+    $Params = ("compile $VERBOSE" -split "\s+") + $BuildFlags
 }
 
 if ($Action -eq "upload") {
-    $Params = "upload --config-dir $CONFIG_DIR $VERBOSE --port $PORT --fqbn $FQBN" -split "\s+"
+    $Params = "upload $VERBOSE" -split "\s+"
 }
 
 if ($Action -eq "monitor") {
-    $Params = "monitor --config-dir $CONFIG_DIR --port $PORT" -split "\s+"
+    $Params = "monitor $PORT_CONFIG" -split "\s+"
 }
 
 iF ($Params.Count -gt 0) {
-    & $ARDUINO_CLI ($Params + $OptParams)
+    & $ARDUINO_CLI ($Params + $BaseParams + $OptParams)
 }
