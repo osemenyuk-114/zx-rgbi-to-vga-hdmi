dc00299a98ca392878ad8695208dfd79243271f6
osemenyuk-114 <oleksandr.semenyuk@hotmail.com>
2026-04-11 01:24:13 +0200

    feat: implement video output type detection in video_output.c and update header

--- ZX_RGBI_TO_VGA_HDMI/ZX_RGBI_TO_VGA_HDMI.ino
+++ ZX_RGBI_TO_VGA_HDMI/ZX_RGBI_TO_VGA_HDMI.ino
@@ -41,6 +41,10 @@ void setup()
   Serial.begin(9600);
 
   load_settings(&settings);
+
+  settings.video_out_type = detect_video_output_type();
+  check_settings(&settings);
+
   set_buffering_mode(settings.buffering_mode);
   draw_welcome_screen(*(video_modes[settings.video_out_mode]));
   set_scanlines_mode();
